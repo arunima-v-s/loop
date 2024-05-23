8ff5781a6c7693f6aca4da58c3fcb971fd3c1a77
arunima-v-s <arunimavishnu36@gmail.com>
2024-05-23 12:34:16 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,28 @@
+
+# count=0
+# while count<=10:
+#     count=count+1
+
+#     if count==6:
+#         continue
+#     print(count)
+
+
+
+# count=1
+# while count<=10:
+#     if count==6:
+#         break
+#     print(count)
+#     count=count+1
+
+count=0
+sum=0
+while count<=10:
+    sum=sum+count
+    count=count+1
+    if count==6:
+        break
+print(sum)
+
+    
