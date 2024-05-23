b71480dcb5a1a97a0abbee0f38d3a75520d97002
arunima-v-s <arunimavishnu36@gmail.com>
2024-05-23 12:30:14 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,68 @@
+# while loop
+
+# a=int(input("enter limit"))
+# count=0
+# while count<a:
+#     print(count)
+#     count=count+1
+
+# a=int(input("enter number"))
+# digit=0
+# while a!=0:
+#     a//=10
+#     digit=digit+1
+# print(digit)
+
+
+# a=int(input("enter first no"))
+# b=int(input("enter second no"))
+# c=input("enter operator")
+# if c=="+":
+#     d=a+b
+#     print(d)
+# elif c=="-":
+#     d=a-b
+#     print(d)
+# elif c=="*":
+#     d=a*b
+#     print(d)
+# elif c=="/":
+#     d=a/b
+#     print(d)
+# else:
+#     pass
+
+# a=int(input("enter first no"))
+# b=int(input("enter second no"))
+# c=int(input("enter third no"))
+# if a<b<c or a>b>c:
+#     print(b,"is median")
+# elif b<a<c or b>a>c:
+#     print(a,"is median")
+# elif a<c<b or a>c>b:
+#     print(c,"is median")
+# else:
+#     pass
+
+
+# a=int(input("enter limit"))
+# for i in range(a+1):
+#     for j in range(i):
+#         print(i,end="")
+#     print("")
+
+
+# a=int(input("enter limit"))
+# for i in range(1,a+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print("")
+
+
+# row=int(input("enter limit"))
+# for i in range(1,row+1):
+#     for j in range(i,row+1):
+#         print(i,end="")
+#     print("")
+
+
